8c7a70f56e8932fff306c7bd8a8ce54ab5d3430e
Nejciny <116084048+Nejciny@users.noreply.github.com>
2023-02-27 14:12:25 +0100

    firebase

---
+++ firebase-config.js
@@ -0,0 +1,21 @@
+import { initializeApp } from "firebase/app";
+import {getFirestore} from "@firebase/firestore";
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDN6JBcJFAn9lJB95OIJ21ef4t1pgxfiFo",
+  authDomain: "store-b59d1.firebaseapp.com",
+  projectId: "store-b59d1",
+  storageBucket: "store-b59d1.appspot.com",
+  messagingSenderId:  "676126030396" ,
+  appId: "1:676126030396:web:f179ac5772050c36bd6139",
+  measurementId: "G-29HRNRFFRW"
+};
+
+
+
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export const db = getFirestore(app);
